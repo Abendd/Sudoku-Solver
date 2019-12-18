796598872cc06998b2671b71418d77fbd75bd4c6
Kartik Tyagi <ktkartiktyagi98@gmail.com>
2019-12-18 10:07:35 +0530

    Fixed error. now Working

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def isSafe(board,row,col,value):
 			if board[i][col]==value:
 				return False
 	for i in range((row//3)*3,(row//3)*3+3):
-		for j in range((col//3)*3,(row//3)*3+3):
+		for j in range((col//3)*3,(col//3)*3+3):
 			if i==row and j==col:
 				continue
 			if board[i][j]==value:
@@ -24,11 +24,11 @@ def nextEmpty(board):
 				return i,j
 	return None
 def solve(board):
-	if nextEmpty(board):
+	if nextEmpty(board)==None:
 		return True
 	else:
-		r,c = nextEmpty
-	print('a:',board[0][0])
+		r,c = nextEmpty(board)
+
 	for i in range(1,10):
 		if isSafe(board,r,c,i):
 			board[r][c]=i
@@ -37,27 +37,6 @@ def solve(board):
 				return True
 			board[r][c]=0
 	return False
-
-def solveBoard():
-	board = [
-		 [0,0,0,0,8,0,0,0,0],
-		 [8,0,9,0,7,1,0,2,0],
-		 [4,0,3,5,0,0,0,0,1],
-		 [0,0,0,1,0,0,0,0,7],
-		 [0,0,2,0,3,4,0,8,0],
-		 [7,3,0,0,0,9,0,0,4],
-		 [9,0,0,0,0,0,7,0,2],
-		 [0,0,8,2,0,5,0,9,0],
-		 [1,0,0,0,4,0,3,0,0]
-		]
-
-	board = solve(board,0,0)
-
-	for i in board:
-		print(i)
-
-
-
 
 board = [
 	 [0,0,0,0,8,0,0,0,0],
